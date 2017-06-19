9fd2c9c0823fdb2c2480a6107ea5d048db2b60f9
leaderbean <634051075@qq.com>
2017-06-19 22:50:00 +0800

    提交代码

---
+++ app/src/main/java/com/king/simplereading/app/App.java
@@ -0,0 +1,14 @@
+package com.king.simplereading.app;
+
+import android.app.Application;
+
+/**
+ * <请描述这个类是干什么的>
+ * Created by adou on 2017/6/19 22 39.
+ *
+ * @Email:634051075@qq.com
+ */
+
+public class App extends Application
+{
+}
--- app/src/main/java/com/king/simplereading/utils/ColorUtil.java
+++ app/src/main/java/com/king/simplereading/utils/ColorUtil.java
@@ -1,6 +1,8 @@
 package com.king.simplereading.utils;
 
 import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v7.graphics.Palette;
 
 /**
  * <请描述这个类是干什么的>
@@ -9,6 +11,21 @@ import android.graphics.Color;
 
 public class ColorUtil
 {
+
+    public static @Nullable
+        Palette.Swatch getMostPopulousSwatch(Palette palette) {
+            Palette.Swatch mostPopulous = null;
+            if (palette != null) {
+                for (Palette.Swatch swatch : palette.getSwatches()) {
+                    if (mostPopulous == null || swatch.getPopulation() > mostPopulous.getPopulation()) {
+                        mostPopulous = swatch;
+                    }
+                }
+            }
+            return mostPopulous;
+        }
+
+
     /**
      * @Description 获取状态栏颜色
      */
